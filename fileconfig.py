4213cd3cfeec91f4f1bcb7b6ff88e87e8f20c7da
Muhammad Fhadli <muhammadfhadli@ymail.com>
2019-09-18 22:53:21 +0900

    Add files via upload

---
+++ fileconfig.py
@@ -0,0 +1,9 @@
+train = {
+	'training_data_dir' : "C:/Users/LENOVO/Jupyter Notebook/Data/Expense/Data dummy Daily Expense.csv",
+	'timestep' : 5
+}
+
+main = {
+	'testing_data_dir' : "C:/Users/LENOVO/Jupyter Notebook/Data/Expense/Data dummy Daily Expense_testing.csv",
+	'model_dir'	: r"C:\Users\LENOVO\Jupyter Notebook\Money Prediction\model.h5"
+}
